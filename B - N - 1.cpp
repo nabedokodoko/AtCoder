78fb8f5dcf06207bfb5bdc435a67f4c678972299
SHUTAPC\sw134 <sw13441206@icloud.com>
2025-11-01 10:46:09 +0900

    B - N - 1

---
+++ B - N - 1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> array;
+
+    int total = 0;
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        array.push_back(a);
+        total += a;
+    }
+
+    bool check = false;
+    for (int i = 0; i < N; i++) {
+        if ((total - array[i]) == M) {
+            check = true;
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    if (check == false) {
+        cout << "No" << endl;
+    }
+}
