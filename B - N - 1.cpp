b5d7dfb14f0a9414dbb74ff65c6b5273ee6df75f
SHUTAPC\sw134 <sw13441206@icloud.com>
2025-11-16 06:51:10 +0900

    関数名変更

--- B - N - 1.cpp
+++ B - N - 1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include <vector>
 using namespace std;
-int main() {
+int atcoder() {
     int N, M;
     cin >> N >> M;
     vector<int> array;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
+
 using namespace std;
 
 int main() {
-	
+	int A, B, C, D;
+	cin >> A >> B >> C >> D;
+
+	if (A <= C) {
+		if (B <= D) {
+			cout << "No" << endl;
+		}
+		else {
+			cout << "Yes" << endl;
+		}
+	}
+	else {
+		cout << "No" << endl;
+	}
+
+
 }
